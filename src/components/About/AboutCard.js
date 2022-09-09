03f794fe61db8d100043532dc5c9049cb3477464
arnab008 <arnab.baksi@arodek.com>
2022-09-09 19:23:37 +0530

    ABOUT CHANGES

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,9 +9,17 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Arnab Baksi </span>
-            from <span className="purple"> Howrah , West Bengal, India.</span>
+            from  Howrah , West Bengal, India.
             <br />I am a M.E.R.N Stack developer.
             <br />
+            <br/>
+          <span style={{fontSize:'25px'}}>  Contact </span><br />
+            <span className="purple">Email </span>: arnabbaksi008@gmail.com<br />
+            <span className="purple">Phone</span> : +91-8777354956<br />
+            <span className="purple">DOB</span> : 26/11/1996<br />
+            <span className="purple">Address</span> : 300 Netaji Subhas Road
+            Howrah - 711101<br />
+
             <br />
             Apart from coding, some other activities that I love to do!
           </p>
