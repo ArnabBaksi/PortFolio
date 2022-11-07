7ab6520a1895a7a991a5ad3e371a0de2fafa1b6a
arnab008 <arnab.baksi@arodek.com>
2022-11-07 23:00:10 +0530

    pdf changes

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,13 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
- import pdf from "../../Assets/../Assets/Arnab-baksi-CV.pdf";
-// import pdf from "../../Assets/../Assets/editable.pdf";
+ import pdf from "../../Assets/../Assets/Arnab-Baksi-CV.pdf";
+// import pdf from "https://drive.google.com/file/d/10bCUmUsmJpP--YJsn0-N9Yj0_B2A1JAa/view?usp=share_link";
+// import pdf from "https://arodekkol-my.sharepoint.com/:b:/g/personal/arnab_baksi_arodek_com/EXfCowar7fdMrbNO-EbXDIgB9B1_nUwS03inGXq5LmY9Fg?e=h2TfIH"
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
-
+// const pdf = "https://github.com/ArnabBaksi/PortFolio/raw/main/src/Assets/Arnab-baksi-CV.pdf"
 
 function ResumeNew() {
   const [width, setWidth] = useState(1300);
@@ -32,12 +33,12 @@ function ResumeNew() {
           </Document>
         
         </Row>
-        <Row className="resume">
-        <Document file={pdf} className="d-flex justify-content-center">
+        {/* <Row className="resume"> */}
+        {/* <Document file={pdf} className="d-flex justify-content-center">
             <Page pageNumber={2} scale={width > 786 ? 1.7 : 0.6} />
         
-          </Document>
-          </Row>
+          </Document> */}
+          {/* </Row> */}
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
